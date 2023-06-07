b4a96f1fc8b834db9554ae04758f6b94db65a63c
Bruno David Thome Hoffmann <brunodh@bempromotora.com.br>
2023-06-07 16:10:44 -0300

    criando o ToString da classe ator

--- backend-entity-mapeando-um-banco-ja-existente/Negocio/Ator.cs
+++ backend-entity-mapeando-um-banco-ja-existente/Negocio/Ator.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace backend_entity_mapeando_um_banco_ja_existente.Negocio
 {
+    [Table("actor")]
     public class Ator
     {
-        
+        [Column("actor_id")]
+        public int Id { get; set; }
+        [Column("first_name")]
+        public string PrimeiroNome { get; set; }
+        [Column("last_name")]
+        public string UltimoNome { get; set; }
+
+        public override string ToString(){
+            return $"Ator ({Id}: {PrimeiroNome} {UltimoNome})";
+        }
     }
 }
--- backend-entity-mapeando-um-banco-ja-existente/Program.cs
+++ backend-entity-mapeando-um-banco-ja-existente/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using backend_entity_mapeando_um_banco_ja_existente.Dados;
+
+namespace backend_entity_mapeando_um_banco_ja_existente.Negocio{
+    class Program{
+        static void Main(string[] args){
+            using (var contexto = new AluraFilmesContexto()){
+                foreach (var ator in contexto.Atores){
+                    Console.WriteLine(ator);
+                }
+            }
+        }
+    }
+}
