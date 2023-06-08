ed70f93e34f61df71325ecf53752f67a78038d7e
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2023-06-08 12:50:19 -0300

    configurando nulidade das colunas

--- backend-entity-mapeando-um-banco-ja-existente/Negocio/Ator.cs
+++ backend-entity-mapeando-um-banco-ja-existente/Negocio/Ator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,8 +12,10 @@ namespace backend_entity_mapeando_um_banco_ja_existente.Negocio
     {
         [Column("actor_id")]
         public int Id { get; set; }
+        [Required]
         [Column("first_name", TypeName = "varchar(45)")]
         public string PrimeiroNome { get; set; }
+        [Required]
         [Column("last_name", TypeName = "varchar(45)")]
         public string UltimoNome { get; set; }
 
