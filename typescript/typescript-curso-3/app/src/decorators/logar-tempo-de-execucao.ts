48f083cb0260d123e6bcac36014256194c739ba6
Bruno David Thome Hoffmann <brunodh@bempromotora.com.br>
2023-08-10 14:23:33 -0300

    terminando curso 3 de typescript

--- typescript/typescript-curso-3/app/src/decorators/logar-tempo-de-execucao.ts
+++ typescript/typescript-curso-3/app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,14 +1,14 @@
-export function logarTempoDeExecucao(emSegundos: boolean = false){
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function(
         target: any,
         propertyKey: string,
         descriptor: PropertyDescriptor
-    ){
-        const metodoOriginal = descriptor.value;    
-        descriptor.value = function(...args: any[]){
+    ) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function(...args: any[]) {
             let divisor = 1;
             let unidade = 'milisegundos';
-            if(emSegundos){
+            if (emSegundos) {
                 divisor = 1000;
                 unidade = 'segundos';
             }
@@ -18,6 +18,7 @@ export function logarTempoDeExecucao(emSegundos: boolean = false){
             console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/divisor} ${unidade}`);
             retorno
         };
+
         return descriptor;
     }
 }
